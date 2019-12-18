1bfd7a894dbd05009494a2107254c63154f1f19f
cs-cognitome <gaya.scienza.v@gmail.com>
2019-12-18 18:30:38 +0200

    Updates

--- precache-manifest.b20e7caff22afef5aeb48d52acac112e.js
+++ precache-manifest.b20e7caff22afef5aeb48d52acac112e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fda25cbe2ba73ad13d871c28e05250bd",
+    "revision": "9e0303bd8ed4698b84b678b638c00118",
     "url": "/react_dynamic-list-of-goods/index.html"
   },
   {
-    "revision": "63d1abebc228003f4d3d",
+    "revision": "9b36e21288f7ca4a117b",
     "url": "/react_dynamic-list-of-goods/static/css/main.36497136.chunk.css"
   },
   {
-    "revision": "b829692f51f58c98b1bd",
-    "url": "/react_dynamic-list-of-goods/static/js/2.d29b3332.chunk.js"
+    "revision": "5a24a4d87377b5283f3c",
+    "url": "/react_dynamic-list-of-goods/static/js/2.4a80f5eb.chunk.js"
   },
   {
-    "revision": "63d1abebc228003f4d3d",
-    "url": "/react_dynamic-list-of-goods/static/js/main.f8590d64.chunk.js"
+    "revision": "9b36e21288f7ca4a117b",
+    "url": "/react_dynamic-list-of-goods/static/js/main.cf819a56.chunk.js"
   },
   {
     "revision": "c4cd083d622a7c9ffb38",
